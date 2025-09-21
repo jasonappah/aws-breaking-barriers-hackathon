a0a7e9be06c25a23edadcd58ad003e5bb628bcb6
Jason Antwi-Appah <hey@jasonaa.me>
2025-09-20 21:16:26 -0500

    chore: use sst router for domain

--- web/sst.config.ts
+++ web/sst.config.ts
@@ -1,63 +1,66 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 export default $config({
-  app(input) {
-    return {
-      name: "aws-breaking-barriers-hackathon",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-      providers: { cloudflare: "6.9.1" },
-    };
-  },
-  async run() {
-    const router = new sst.aws.Router("MyRouter", {
-      domain: {
-        name: "swarm.jasonaa.me",
-        aliases: ["*.swarm.jasonaa.me"],
-        dns: sst.cloudflare.dns(),
-      },
-    });
-    const vpc = new sst.aws.Vpc("Vpc", { bastion: true, nat: "ec2" });
-    const rds = new sst.aws.Postgres("Postgres", {
-      vpc,
-      proxy: true,
-      dev: {
-        username: "postgres",
-        password: "password",
-        database: "postgres",
-        port: 5432,
-      },
-    });
-    const server = new sst.aws.Function("server", {
-      url: {
-        router: {
-          instance: router,
-          domain: "api.swarm.jasonaa.me",
-        },
-      },
-      handler: "apps/server/src/lambda.handler",
-      link: [rds],
-      vpc,
-    });
-    const web = new sst.aws.StaticSite("web", {
-      path: "apps/web",
-      dev: {
-        command: "npm run dev",
-      },
-      build: {
-        command: "npm run build",
-        output: "dist",
-      },
-      domain: "swarm.jasonaa.me",
-      environment: {
-        VITE_SERVER_URL: server.url,
-      },
-    });
-    new sst.x.DevCommand("Studio", {
-      link: [rds],
-      dev: {
-        command: "npx drizzle-kit studio",
-      },
-    });
-  },
+	app(input) {
+		return {
+			name: "aws-breaking-barriers-hackathon",
+			removal: input?.stage === "production" ? "retain" : "remove",
+			protect: ["production"].includes(input?.stage),
+			home: "aws",
+			providers: { cloudflare: "6.9.1" },
+		};
+	},
+	async run() {
+		const router = new sst.aws.Router("MyRouter", {
+			domain: {
+				name: "swarm.jasonaa.me",
+				aliases: ["*.swarm.jasonaa.me"],
+				dns: sst.cloudflare.dns(),
+			},
+		});
+		const vpc = new sst.aws.Vpc("Vpc", { bastion: true, nat: "ec2" });
+		const rds = new sst.aws.Postgres("Postgres", {
+			vpc,
+			proxy: true,
+			dev: {
+				username: "postgres",
+				password: "password",
+				database: "postgres",
+				port: 5432,
+			},
+		});
+		const server = new sst.aws.Function("server", {
+			url: {
+				router: {
+					instance: router,
+					domain: "api.swarm.jasonaa.me",
+				},
+			},
+			handler: "apps/server/src/lambda.handler",
+			link: [rds],
+			vpc,
+		});
+		const web = new sst.aws.StaticSite("web", {
+			path: "apps/web",
+			dev: {
+				command: "npm run dev",
+			},
+			build: {
+				command: "npm run build",
+				output: "dist",
+			},
+			router: {
+				instance: router,
+				domain: "swarm.jasonaa.me",
+			},
+			environment: {
+				VITE_SERVER_URL: server.url,
+			},
+		});
+		new sst.x.DevCommand("Studio", {
+			link: [rds],
+			dev: {
+				command: "npx drizzle-kit studio",
+			},
+		});
+	},
 });
