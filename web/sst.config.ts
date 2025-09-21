d85eee771f8de50c9ae203d742806130b97fc975
Jason Antwi-Appah <hey@jasonaa.me>
2025-09-21 07:45:01 -0500

    ci: readd mapbox token

--- web/sst.config.ts
+++ web/sst.config.ts
@@ -42,7 +42,6 @@ export default $config({
 			link: [rds],
 			vpc,
 		});
-		// const mapboxToken = new sst.Secret("MapBoxToken");
 		const web = new sst.aws.StaticSite("web", {
 			path: "apps/web",
 			dev: {
@@ -58,7 +57,7 @@ export default $config({
 			},
 			environment: {
 				VITE_SERVER_URL: server.url,
-				// VITE_MAPBOX_TOKEN: new sst.Secret("MapBoxToken").value,
+				VITE_MAPBOX_TOKEN: new sst.Secret("MapBoxToken").value,
 			},
 		});
 		new sst.x.DevCommand("Studio", {
